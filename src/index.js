315a40297d284e24d004487c41a3c1c48e713270
jatin bhanot <jatinbhanot@nicobar.com>
2020-07-08 21:31:59 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (module.hot) { module.hot.accept(App);}
